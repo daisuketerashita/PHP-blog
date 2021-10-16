be7cf8d1235dbe408a918c45051fee2150de19fc
daisuketerashita <ekusiad.tera@icloud.com>
2021-10-16 22:59:48 +0900

    編集機能の実装

--- lib/queryArticle.php
+++ lib/queryArticle.php
@@ -13,7 +13,16 @@ class QueryArticle extends Connect{
 
     public function save(){
         if($this->article->getId()){
-            //idがあるときは上書き
+            //IDがあるときは上書き
+            $id = $this->article->getId();
+            $title = $this->article->getTitle();
+            $body = $this->article->getBody();
+            $stmt = $this->dbh->prepare("UPDATE articles
+                SET title=:title, body=:body, updated_at=NOW() WHERE id=:id");
+            $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+            $stmt->bindParam(':body', $body, PDO::PARAM_STR);
+            $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt->execute();
         }else{
             // IDがなければ新規作成
             $title = $this->article->getTitle();
@@ -24,6 +33,23 @@ class QueryArticle extends Connect{
             $stmt->bindParam(':body', $body, PDO::PARAM_STR);
             $stmt->execute();
         }
+    }
+
+    public function find($id){
+        $stmt = $this->dbh->prepare("SELECT * FROM articles WHERE id=:id");
+        $stmt->bindParam(':id',$id,PDO::PARAM_INT);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $article = null;
+        if ($result){
+            $article = new Article();
+            $article->setId($result['id']);
+            $article->setTitle($result['title']);
+            $article->setBody($result['body']);
+            $article->setCreatedAt($result['created_at']);
+            $article->setUpdatedAt($result['updated_at']);
+        }
+        return $article;
     }
 
     public function findAll(){
